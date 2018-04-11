2ad0550f9e9283bf28d24587c2028e1b818e8c0a
hod <hody92@icloud.com>
2018-04-11 17:18:35 +0300

    test

--- main.js
+++ main.js
@@ -2,19 +2,56 @@ var coffeeShop = {
   beans: 40,
 
   drinkRequirements: {
-    latte: 10,
-    americano: 5,
-    doubleShot: 15,
-    frenchPress: 12
+    latte:{
+    beansNeeded: 10,
+    price: 4.45,
+    },
+    americano:{
+    beansNeeded: 5,
+    price: 6.4
+    },
+    doubleShot:{
+    beansNeeded: 15,
+    price: 5
+    },
+    frenchPress:{
+    beansNeeded: 12,
+    price: 6.5
+    }
+  },
+  buyDrink: function drinkBuy (drinkType){
+      if (drinkType in this.drinkRequirements ){
+        this.money = this.money += this.drinkRequirements[drinkType].price;
+        this.makeDrink(drinkType);
+        console.log('coffeeShop has ' + coffeeShop.money + ' shekels')
+      }else{
+        console.log("Sorry, we don't make " + drinkType)
+    }
+  },
+  money: 100,
+  buy: function buyBeans(num){
+    this.money -= num;
+    this.beans += num;
+    console.log('you bought ' + num + ' beans for ' + num + ' shekels')
+    console.log('you now have ' + this.beans + ' beans and ' + this.money + ' shekels')
   },
 
   makeDrink: function (drinkType) {
-    // TODO: Finish this method
+    if (drinkType in this.drinkRequirements) {
+      if (coffeeShop.beans > coffeeShop.drinkRequirements[drinkType].beansNeeded){
+      coffeeShop.beans -= coffeeShop.drinkRequirements[drinkType].beansNeeded
+      console.log('one ' + drinkType + ' on its way')
+    }else if (drinkType in this.drinkRequirements === false) {
+      console.log("Sorry, we don't make " + drinkType)
+    }else if (coffeeShop.beans > coffeeShop.drinkRequirements[drinkType].beansNeeded === false){
+      console.log('sorry we dont have the required amount of beans')
+      console.log(coffeeShop.beans + ' beans left')
+    }
   }
 }
-
-coffeeShop.makeDrink("latte");
-coffeeShop.makeDrink("americano");
-coffeeShop.makeDrink("filtered");
-coffeeShop.makeDrink("doubleShot");
-coffeeShop.makeDrink("frenchPress");
+}
+coffeeShop.buyDrink("latte");
+coffeeShop.buyDrink("americano");
+coffeeShop.buyDrink("filtered");
+coffeeShop.buyDrink("doubleShot");
+coffeeShop.buyDrink("frenchPress");
